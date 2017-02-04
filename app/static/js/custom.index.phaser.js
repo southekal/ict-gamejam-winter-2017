53aabe25bdc9865e7d2a737270782174cbc7fc83
Sandip Southekal <southekal@gmail.com>
2017-02-04 10:55:33 -0600

    second day commits

--- app/static/js/custom.index.phaser.js
+++ app/static/js/custom.index.phaser.js
@@ -17,6 +17,7 @@ window.onload = function() {
         game.load.image('sky', "static/img/intro/sky.png");
         game.load.image('ground', "static/img/intro/platform.png");
         game.load.image('star', "static/img/intro/star.png");
+        game.load.image('bullet', "static/img/intro/bullet.png");
         game.load.spritesheet('dude', "static/img/intro/dude.png", 32, 48);
 
     }
@@ -31,7 +32,6 @@ window.onload = function() {
     var scoreTime;
     var today = new Date();
 
-    var ship;
     var weapon;
     var fireButton;
 
@@ -39,20 +39,101 @@ window.onload = function() {
     var weaponCounterText;
 
     function create () {
-
         //  We're going to be using physics, so enable the Arcade Physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
-
         //  A simple background for our game
         game.add.sprite(0, 0, 'sky');
-
         //  The platforms group contains the ground and the 2 ledges we can jump on
         platforms = game.add.group();
-
         //  We will enable physics for any object that is created in this group
         platforms.enableBody = true;
+        // Set up ground section
+        setupGround();
+        // Set up the ledges
+        setupLedge();
+        // Set up the player
+        setupPlayer();
+        // Creates random stars
+        setupStars();
+        //  The score
+        scoreText = game.add.text(16, 16, 'Score Mofo: 0', { fontSize: '32px', fill: '#000' });
+        //  The score
+        weaponCounterText = game.add.text(16, 52, 'Weapon Mofo: 0', { fontSize: '32px', fill: '#000' });
+        //  Our controls.
+        cursors = game.input.keyboard.createCursorKeys();
+        // Create weapon and attach it to player
+        createWeaponTracker();
+        fireButton = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+        // Function to go full screen
+        //game.input.onDown.add(goFullscreen, this);
+    }
 
-        // Here we create the ground.
+    function update() {
+
+        //  Collide the player and the stars with the platforms
+        game.physics.arcade.collide(player, platforms);
+        game.physics.arcade.collide(stars, platforms);
+
+        //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+        game.physics.arcade.overlap(player, stars, collectStar, null, this);
+
+        //  Reset the players velocity (movement)
+        player.body.velocity.x = 0;
+        player.body.velocity.y = 0;
+
+        if (cursors.left.isDown)
+        {
+            //  Move to the left
+            player.body.velocity.x = -150;
+            player.animations.play('left');
+        }
+        else if (cursors.right.isDown)
+        {
+            //  Move to the right
+            player.body.velocity.x = 150;
+            player.animations.play('right');
+        }
+        else
+        {
+            //  Stand still
+            player.animations.stop();
+            player.frame = 4;
+        }
+
+        if (cursors.up.isDown)
+        {
+            player.body.velocity.y = -200;
+        }
+        else if (cursors.down.isDown)
+        {
+            player.body.velocity.y = 200;
+        }
+
+
+        if (fireButton.isDown)
+        {
+            // checks for spacebar keyboard event
+            game.input.keyboard.onDownCallback = function() {
+                if (game.input.keyboard.event.keyCode == 32){
+                    checkFireButton(weaponCounter);
+                }
+            };
+        }
+
+        // This will take the given game object and check if its x/y coordinates fall outside of the world bounds.
+        game.world.wrap(player, 16);
+
+    }
+
+    function render() {
+        weapon.debug();
+    }
+
+//*********************************************************************************************************************
+//    CUSTOM FUNCTIONS
+
+    function setupGround() {
+       // Here we create the ground.
         var ground = platforms.create(0, game.world.height - 64, 'ground');
 
         //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
@@ -60,7 +141,9 @@ window.onload = function() {
 
         //  This stops it from falling away when you jump on it
         ground.body.immovable = true;
+    }
 
+    function setupLedge() {
         //  Now let's create two ledges
         var ledge = platforms.create(400, 400, 'ground');
 
@@ -69,9 +152,11 @@ window.onload = function() {
         ledge = platforms.create(-150, 250, 'ground');
 
         ledge.body.immovable = true;
+    }
 
+    function setupPlayer() {
         // The player and its settings
-        player = game.add.sprite(60, game.world.height - 150, 'dude');
+        player = game.add.sprite(60, game.world.height - 88, 'dude');
 
         //  We need to enable physics on the player
         game.physics.arcade.enable(player);
@@ -79,189 +164,61 @@ window.onload = function() {
         //  Player physics properties. Give the little guy a slight bounce.
         player.body.bounce.y = 0.2;
         player.body.gravity.y = 300;
+        player.body.allowGravity = false;
         player.body.collideWorldBounds = true;
 
         //  Our two animations, walking left and right.
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+        player.anchor.set(0.5)
+
+    }
+
+    function setupStars() {
         //  Finally some stars to collect
         stars = game.add.group();
-
         //  We will enable physics for any star that is created in this group
         stars.enableBody = true;
-
-        //  Here we'll create 12 of them evenly spaced apart
+        //  Here we'll create 10 of them evenly spaced apart
         for (var i = 0; i < 12; i++)
         {
             //  Create a star inside of the 'stars' group
             var star = stars.create(i * 70, 0, 'star');
-
             //  Let gravity do its thing
-            star.body.gravity.y = 300;
-
+            star.body.gravity.y = 600;
             //  This just gives each star a slightly random bounce value
             star.body.bounce.y = 0.7 + Math.random() * 0.2;
         }
+    }
 
-        //  The score
-        scoreText = game.add.text(16, 16, 'Score Mofo: 0', { fontSize: '32px', fill: '#000' });
-
-        //  The score
-        weaponCounterText = game.add.text(16, 52, 'Weapon Counter: 0', { fontSize: '32px', fill: '#000' });
-
-        //  Our controls.
-        cursors = game.input.keyboard.createCursorKeys();
-
-
-
-
-
-
-
-       //  Creates 30 bullets, using the 'bullet' graphic
-        weapon = game.add.weapon(1, 'star');
-
+    function createWeaponTracker() {
+             //  Creates 30 bullets, using the 'bullet' graphic
+        weapon = game.add.weapon(30, 'bullet');
         //  The bullet will be automatically killed when it leaves the world bounds
         weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-
+        //  Because our bullet is drawn facing up, we need to offset its rotation:
+        weapon.bulletAngleOffset = 90;
         //  The speed at which the bullet is fired
-        weapon.bulletSpeed = 600;
-
+        weapon.bulletSpeed = 400;
         //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
-        weapon.fireRate = 100;
-
-        ship = this.add.sprite(400, 300, 'dude');
-
-        ship.anchor.set(0.5);
-
-        game.physics.arcade.enable(ship);
-
-        //ship.body.drag.set(70);
-        //ship.body.maxVelocity.set(200);
-
-        ship.body.bounce.y = 0.2;
-        ship.body.gravity.y = 300;
-        ship.body.collideWorldBounds = true;
-
-        //  Our two animations, walking left and right.
-        ship.animations.add('left', [0, 1, 2, 3], 10, true);
-        ship.animations.add('right', [5, 6, 7, 8], 10, true);
-
+        weapon.fireRate = 60;
         //  Tell the Weapon to track the 'player' Sprite
         //  With no offsets from the position
         //  But the 'true' argument tells the weapon to track sprite rotation
-        weapon.trackSprite(ship, 0, 0, true);
-
-        fireButton = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-
-        //game.input.onDown.add(goFullscreen, this);
-
-        function goFullscreen() {
-            game.scale.fullScreenScale = Phaser.ScaleManager.SHOW_ALL;
-            game.scale.startFullScreen();
-        }
-
-
+        weapon.trackSprite(player, 0, 0);
     }
 
-    function update() {
-
-        //  Collide the player and the stars with the platforms
-        game.physics.arcade.collide(player, platforms);
-        game.physics.arcade.collide(stars, platforms);
-        game.physics.arcade.collide(ship, platforms);
-
-        //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-        game.physics.arcade.overlap(player, stars, collectStar, null, this);
-        game.physics.arcade.overlap(ship, stars, collectStar, null, this);
-
-        //  Reset the players velocity (movement)
-        player.body.velocity.x = 0;
-
-        if (cursors.left.isDown)
-        {
-            //  Move to the left
-            player.body.velocity.x = -150;
-
-            player.animations.play('left');
+    function checkFireButton(val) {
+        if (val > 0) {
+            weapon.fire();
+            weaponCounter -= 1;
+            updateWeaponCounter(weaponCounter);
         }
-        else if (cursors.right.isDown)
-        {
-            //  Move to the right
-            player.body.velocity.x = 150;
-
-            player.animations.play('right');
-        }
-        else
-        {
-            //  Stand still
-            player.animations.stop();
-
-            player.frame = 4;
-        }
-
-        //  Allow the player to jump if they are touching the ground.
-        if (cursors.up.isDown && player.body.touching.down)
-        {
-            player.body.velocity.y = -350;
-        }
-
-
-
-
-        //  Reset the players velocity (movement)
-        ship.body.velocity.x = 0;
-
-        if (cursors.up.isDown)
-        {
-            //game.physics.arcade.accelerationFromRotation(ship.rotation, 300, ship.body.acceleration);
-            ship.body.velocity.y = -350;
-        }
-        else
-        {
-            ship.body.acceleration.set(0);
-        }
-
-        if (cursors.left.isDown)
-        {
-            //ship.body.angularVelocity = -300;
-            ship.body.velocity.x = -150;
-            ship.animations.play('left');
-        }
-        else if (cursors.right.isDown)
-        {
-            //ship.body.angularVelocity = 300;
-            ship.body.velocity.x = 150;
-            ship.animations.play('right');
-        }
-        else
-        {
-           // ship.body.angularVelocity = 0;
-            ship.animations.stop();
-            ship.frame = 4;
-        }
-
-        if (fireButton.isDown)
-        {
-            checkFireButton(weaponCounter);
-        }
-
-        function checkFireButton(val) {
-            if (val > 0) {
-                weapon.fire();
-                weaponCounter = val - 1;
-                console.log(weaponCounter);
-                updateWeaponCounter(weaponCounter);
-            }
-        }
-
-        game.world.wrap(ship, 16);
-
     }
 
     function updateWeaponCounter(val){
-        weaponCounterText.text = 'Weapons: ' + val;
+        weaponCounterText.text = 'Weapons Mofo: ' + val;
     }
 
 
@@ -271,15 +228,17 @@ window.onload = function() {
 
         //  Add and update the score
         score += 10;
-        weaponCounter += 1;
-
         scoreText.text = 'Score Mofo: ' + score;
+
+        weaponCounter += 1;
         updateWeaponCounter(weaponCounter);
     }
 
-    function render() {
-        weapon.debug();
+    function goFullscreen() {
+        game.scale.fullScreenScale = Phaser.ScaleManager.SHOW_ALL;
+        game.scale.startFullScreen();
     }
 
+//*********************************************************************************************************************
 
 };
