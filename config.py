8c0622085dc69e740e3590eff27336757f23491c
Sandip Southekal <southekal@gmail.com>
2017-02-05 04:22:10 -0600

    read from env var

--- config.py
+++ config.py
@@ -4,18 +4,18 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 cfg = ConfigParser.ConfigParser()
-cfg.read('secret.cfg')
+# cfg.read('secret.cfg')
 
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = cfg.get("SECRET", "SECRET_TOKEN")
+    SECRET_KEY = os.environ.get("SECRET_TOKEN")
     THREADS_PER_PAGE = 2
     # Use a secure, unique and absolutely secret key for
     # signing the data.
-    CSRF_SESSION_KEY = cfg.get("SECRET", "CSRF_SECRET_TOKEN")
+    CSRF_SESSION_KEY = os.environ.get("CSRF_SECRET_TOKEN")
 
 
 class ProductionConfig(Config):
